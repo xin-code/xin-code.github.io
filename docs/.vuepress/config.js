c6ad72ed29f58215a1a4ba5f1f1e8ad8a89a9028
DESKTOP-XIN\Xin <975203723@qq.com>
2021-07-09 15:41:20 +0800

    feat: :tada: init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,42 +1,28 @@
 module.exports = {
-  title: 'Xin-code',
-  dest: './dist',
-  port: '8888',
-  head: [
-      ['link', {rel: 'icon', href: '/logo.jpg'}]
-  ],
-  markdown: {
-      lineNumbers: true
-  },
+  title: 'Xin', 
+  description: '个人博客',
   themeConfig: {
-      nav:require('./nav'),
-      sidebar: {'/guide/':[
-          {
-                title:'新手指南',
-                collapsable: true,
-                children:[
-                  '/guide/notes/one',
-                ]
-              },
-              {
-                title:'知码学院',
-                collapsable: true,
-                children:[
-                  '/guide/notes/two',
-                ]
-              }
+      search: true,//搜索
+      searchMaxSuggestions: 10,
+      // 导航栏
+      nav: [
+        { text: '首页', link: '/' },
+        { text: '前端技术', link: 'index/' },
+        { text: '个人文章', items: [
+          { text: '掘金', link: 'https://juejin.cn/user/405039088151837' },
+          { text: 'GitHub', link: 'https://github.com/xin-code' },
+        ]}
+      ],
+      // 侧边栏
+      sidebar: [
+        {
+          title: 'HTML',
+          collapsable: true,
+          children: [
+            'index/',
+            'index/第一个',
           ]
-      },
-      sidebarDepth: 2,
-      lastUpdated: 'Last Updated',
-      searchMaxSuggestoins: 10,
-      serviceWorker: {
-          updatePopup: {
-              message: "有新的内容.",
-              buttonText: '更新'
-          }
-      },
-      editLinks: true,
-      editLinkText: '在 GitHub 上编辑此页 ！'
+        }
+      ],
+    }
   }
-}
