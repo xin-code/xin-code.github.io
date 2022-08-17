e1dc8161af3da55c30ae8fa1bca5f220c646de11
Xin <975203723@qq.com>
2022-08-17 10:43:40 +0800

    fix(common): :bug: 语言配置

--- blog/.vuepress/config.ts
+++ blog/.vuepress/config.ts
@@ -3,7 +3,11 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme";
 
 export default defineUserConfig({
-  lang: "zh-CN",
+  locales: {
+    "/zh/": {
+      lang: "zh-CN",
+    },
+  },
   title: "前端札记",
   description: "个人的前端笔记",
 
