aa043d041330b981050f43a0745653996202f775
Xin <975203723@qq.com>
2025-01-10 13:54:56 +0800

    feat: :sparkles: 添加vue-katex组件

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,7 +1,9 @@
 // import vue from 'vue/dist/vue.esm.browser'
 import ElementUI from "element-ui"
 import Clipboard from 'v-clipboard'
+import VueKatex from 'vue-katex';
 import "element-ui/lib/theme-chalk/index.css"
+import 'katex/dist/katex.min.css';
 
 export default ({
   Vue, // VuePress 正在使用的 Vue 构造函数
@@ -11,5 +13,6 @@ export default ({
 }) => {
   Vue.use(ElementUI)
   Vue.use(Clipboard)
+  Vue.use(VueKatex)
   // window.Vue = vue; // 使页面中可以使用Vue构造函数 （使页面中的vue demo生效）
 }
